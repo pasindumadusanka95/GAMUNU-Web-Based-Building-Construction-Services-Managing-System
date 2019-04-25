2df7afb65231255e68718d1add24b0b5b9cc74d6
ayeshNipun <nipun.yesh@gmail.com>
2019-04-25 19:04:26 +0530

    login ui customization

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { NgModel } from '@angular/forms';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+// import { NgModel } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+  @Input() email: string;
+  @Input() password: string;
+  constructor() { }
 
   ngOnInit() {
+	  console.log(this.email)
   }
   ngOnDestroy() {
   }
