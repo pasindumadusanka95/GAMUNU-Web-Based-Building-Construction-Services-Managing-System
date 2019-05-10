f6109daa10bc4307d98e6b63cb0340467abcfdeb
ayeshNipun <nipun.yesh@gmail.com>
2019-05-10 13:15:46 +0530

    homepage stats

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { WorkerService } from 'src/app/shared/worker.service';
+import { ProjectService } from 'src/app/shared/project.service';
+import { OrderService } from 'src/app/shared/order.service';
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+	workers: number;
+	projects: number;
+	orders: number;
+
+  constructor(private workerService: WorkerService, private projectService: ProjectService, private orderService: OrderService) { }
 
   ngOnInit() {
+	this.getWorkerCount();
+	this.getProjectCount();
+	this.getOrderCount();
+  }
+
+  getWorkerCount(){
+	this.workerService.getWorkerList().subscribe((res) => {
+		this.workers = Object.keys(res).length;
+	})
+  }
+
+  getProjectCount(){
+	this.projectService.getProjectList().subscribe((res) => {
+		this.projects = Object.keys(res).length;
+	})
+  }
+  getOrderCount(){
+	this.orderService.getOrderList().subscribe((res) => {
+		this.orders = Object.keys(res).length;
+	})
   }
 
 }
