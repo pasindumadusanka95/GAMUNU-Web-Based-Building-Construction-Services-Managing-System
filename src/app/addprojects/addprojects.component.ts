337445f4f6c9bbc3b7f93ba231caf3bda8d76890
Pasindu Madusanka <pasindusenerath@gmail.com>
2019-06-08 07:22:57 +0530

    create code structures and clean garbage codes

--- src/app/addprojects/addprojects.component.ts
+++ src/app/addprojects/addprojects.component.ts
@@ -12,7 +12,7 @@ import { ToastrService } from 'ngx-toastr';
 export class AddprojectsComponent implements OnInit {
   @ViewChild('projectForm') project_form;
 
-  projects=['Completed','Not Compeleted','Ongoing','Finished','Canceled'];
+  projects = ['Completed', 'Not Compeleted', 'Ongoing', 'Finished', 'Canceled'];
   constructor(public projectService: ProjectService,
     private toastr: ToastrService) { }
 
@@ -20,53 +20,52 @@ export class AddprojectsComponent implements OnInit {
     this.resetForm(this.project_form);
     this.refreshProjectList();
   }
-  resetForm(form?: NgForm){
-    if(form){
+  resetForm(form?: NgForm) {
+    if (form) {
       form.reset();
       this.projectService.selectedProject = {
-      _id: '',
-      project_id: null,
-      order_id: null,
-      project_name: '',
-      start_date: '',
-      end_date: '',
-      project_loc:'',
-      cus_name:'',
-      project_status:'',
-      project_cost:null,
-      worker_count: null,
+        _id: '',
+        project_id: null,
+        order_id: null,
+        project_name: '',
+        start_date: '',
+        end_date: '',
+        project_loc: '',
+        cus_name: '',
+        project_status: '',
+        project_cost: null,
+        worker_count: null,
 
 
       };
     }
   }
-  refreshProjectList(){
-    this.projectService.getProjectList().subscribe((res)=>{
+  refreshProjectList() {
+    this.projectService.getProjectList().subscribe((res) => {
       this.projectService.projects = res as Project[];
     });
   }
-  onSubmit(form : NgForm){
-    if(form.value._id==''){
-    this.projectService.postProject(form.value).subscribe((res)=>{
-      this.resetForm(form);
-      this.refreshProjectList();
-      this.toastr.success('Submiited Successfully', 'Project Data');
-    });
-  } else{
-  // tslint:disable-next-line: deprecation
-    this.projectService.putProject(form.value).subscribe((res)=>{
-      this.resetForm(form);
-      this.refreshProjectList();
-      this.toastr.info('Updated Successfully', 'Project Data');
-    });
+  onSubmit(form: NgForm) {
+    if (form.value._id == '') {
+      this.projectService.postProject(form.value).subscribe((res) => {
+        this.resetForm(form);
+        this.refreshProjectList();
+        this.toastr.success('Submiited Successfully', 'Project Data');
+      });
+    } else {
+      // tslint:disable-next-line: deprecation
+      this.projectService.putProject(form.value).subscribe((res) => {
+        this.resetForm(form);
+        this.refreshProjectList();
+        this.toastr.info('Updated Successfully', 'Project Data');
+      });
+    }
   }
-}
-onEdit(pro:Project){
-  this.projectService.selectedProject = pro;
-}
+  onEdit(pro: Project) {
+    this.projectService.selectedProject = pro;
+  }
 
-onChange(newValue) {
-  // console.log(newValue);
-  this.projectService.selectedProject.project_status = newValue;
-}
+  onChange(newValue) {
+    this.projectService.selectedProject.project_status = newValue;
+  }
 }
